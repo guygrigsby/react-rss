9fc3a6311c5942ae866bdb60395d621a5680f7d9
Guy J Grigsby <guy.grigsby@gmail.com>
2020-07-17 19:00:45 -0600

    forgot date code. remove extra file for reading local files'

--- src/components/FeedHeader.js
+++ src/components/FeedHeader.js
@@ -9,27 +9,36 @@ const FeedHeader = (props) => {
     item.getElementsByTagName('dc:creator').item(0) &&
     item.getElementsByTagName('dc:creator').item(0).childNodes[0].nodeValue
 
+  const publishDate = new Date(item.querySelector('pubDate').innerHTML)
+
   return (
     <div className="d-flex flex-row" id={`heading${id}`}>
       <div className="m-2 p-2">{togglePaneElement}</div>
       <div className="d-flex flex-column w-100" id={`heading${id}`}>
         <div className="d-flex flex-row">
           <h5 className="ml-1">{item.querySelector('title').innerHTML}</h5>
-          <small className="m-1 ml-2">
-            {item.querySelector('pubDate').innerHTML}
-          </small>
+          <small className="m-1 ml-2">{publishDate.toLocaleString()}</small>
           <span className="ml-auto">
             <Favorite {...props} />
           </span>
         </div>
         {creator ? (
           <>
-            <small>by {creator}</small>
+            <small className="">by {creator}</small>
           </>
         ) : null}
       </div>
     </div>
   )
+}
+
+// parse a date format of
+// Fri, 17 Jul 2020 10:23:00 +0000
+function parseDate(str) {
+  let p = str.split(' ')
+
+  // new Date(year, month [, day [, hours[, minutes[, seconds[, ms]]]]])
+  return new Date(p[3], p[2])
 }
 
 FeedHeader.propTypes = {
--- src/services/file.js
+++
@@ -1,7 +0,0 @@
-const readFileAsString = (path) => {
-  var reader = new FileReader();
-  reader.onload = function (event) {
-    console.log('File content:', event.target.result);
-  };
-  reader.readAsText(files[0]);
-};
